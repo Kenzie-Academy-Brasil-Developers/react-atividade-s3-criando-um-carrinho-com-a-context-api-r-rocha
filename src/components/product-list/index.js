44ad91aad1d069a2815900ecd5c0f2f43509e6c1
Roberto Rocha <rsrocha@usp.br>
2021-10-10 17:47:11 -0300

    feat: implementation of businessess rules

---
+++ src/components/product-list/index.js
@@ -0,0 +1,30 @@
+import Button from "../button";
+
+import { useContext } from "react";
+
+import { CatalogueContext } from "../../providers/catalogue";
+import  { CartContext } from "../../providers/cart";
+
+
+const ProductList = ({ type }) => {
+    const { catalogue } = useContext(CatalogueContext);
+    const { cart } = useContext(CartContext);
+
+    return (
+        <ul>
+            {type === "catalogue" && catalogue.map((item, index) => (
+                <li key={index}>
+                    {item.name} <Button type={type} item={item} />
+                </li>
+            ))}
+
+            {type === "cart" && cart.map((item, index) => (
+                <li key={index}>
+                    {item.name} <Button type={type} item={item} />
+                </li>
+            ))}
+        </ul>
+    )
+}
+
+export default ProductList;
